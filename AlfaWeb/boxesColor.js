5d5a183f9095dbba5c8d34559e949736b3f75262
Eztbam <eztbam19c@gmail.com>
2019-06-18 16:18:44 -0600

    cambios a boxesColor

--- AlfaWeb/boxesColor.js
+++ AlfaWeb/boxesColor.js
@@ -1,22 +1,22 @@
 const color = ['#616DB2', '#566EB3', '#3E55A4', '#7481BF'];
-
 let cuadrado = document.querySelectorAll('.square');
+var auxAnterior;    
+let randomColor = color[Math.floor(Math.random() * color.length)];
+//se llena el primer color
+auxAnterior = randomColor;
+cuadrado[0].style.backgroundColor = randomColor;
 
-let FirtColor;
-
-for (let i = 0; i < cuadrado.length; i++) {
-    
-    
-    let randomColor = color[Math.floor(Math.random() * color.length)];
-
-        
-            cuadrado[i].style.backgroundColor = randomColor;
-   
-    
-
+for (let i = 1; i < cuadrado.length; i++) {
+    //cambio de color
+    randomColor = color[Math.floor(Math.random() * color.length)];
+    //Si el color del cuadrado anterior es igual ejecuta el ciclo
+    while (auxAnterior == randomColor){
+        randomColor = color[Math.floor(Math.random() * color.length)];
+    }
+    //guardamos variables
+    cuadrado[i].style.backgroundColor = randomColor;
+    auxAnterior = randomColor;
 }
-
-
 
 
 
